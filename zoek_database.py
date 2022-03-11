6918a40d3f3246847d8e6837012a8767c9aa98af
Britt vink <brittvink@Britts-MacBook-Air.local>
2022-03-11 16:33:17 +0100

    refactoring and comments

--- zoek_database.py
+++ zoek_database.py
@@ -2,8 +2,8 @@
 
 """
 File:         Database.py
-Created:      2022/02/17
-Last Changed: 2022/02/22
+Created:      2022/03/08
+Last Changed: 2022/03/11
 Author:       B.Vink
 
 This pythonscript is used to read a text file and put the data in a MySQL Database
@@ -19,7 +19,7 @@ from mysql.connector import connect, Error
 import uuid
 
 # Metadata
-__program__ = "fill database"
+__program__ = "fill filters"
 __author__ = "Britt Vink"
 __maintainer__ = "Britt Vink"
 __email__ = "b.vink@st.hanze.nl"
@@ -82,22 +82,42 @@ class main():
                 # get cursor object
                 cursor = connection.cursor()
 
-                id_voor_db = uuid.uuid1()
-                print(str(id_voor_db.int))
 
-                sql = "select * from database_Information where title like '%" + self.input_word + "%' or summary like '%" + self.input_word + "%'"
+                # Find the vocalulaire of the input word and make a list of the result
+                sql = "select word from database_Vocabulair where key_id ='" + self.input_word +  "'"
                 cursor.execute(sql)
-                row = [item[-1] for item in cursor.fetchall()]
+                vocabulair_list = [item[-1] for item in cursor.fetchall()]
+                vocabulair_list.append(self.input_word)
 
-                for article in row:
-                    print(type(article))
+
+                # Try to find articles that match
+                # And make a list of the results
+                list_with_articles = []
+                for word in vocabulair_list:
+                    sql = "select * from database_Information where title like '%" \
+                          + word +\
+                          "%' or title like '%" \
+                          + word.capitalize() +\
+                          "%'or summary like '%" \
+                          + word + \
+                          "%'or summary like '%" \
+                        + word.capitalize() + "%'"
+                    cursor.execute(sql)
+                    list_of_articles = [item[-1] for item in cursor.fetchall()]
+                    list_with_articles.append(list_of_articles)
+
+                # Make one list of list of lists
+                list_with_articles = [item for sublist in list_with_articles for item in sublist]
+                ## Delete duplicates from the list
+                list_with_articles = list(set(list_with_articles))
+
+                # Add articles to database
+                for article in list_with_articles:
                     id = uuid.uuid1()
                     id_num = id.int
-                    sql = 'INSERT INTO KCBBE2.database_Search (id, search_id, article_id) VALUE ("' + str(id_num) + '", "' + self.input_word + '", "' + article + '")'
-                    print(sql)
+                    sql = 'INSERT INTO KCBBE2.database_Search (id, search_id, article_id) VALUE("' + str(id_num) + '", "' + self.input_word + '", "' + article + '")'
                     cursor.execute(sql)
                     connection.commit()
-
 
                 # Close connection
                 cursor.close()
@@ -110,7 +130,7 @@ class main():
 
     def print_arguments(self):
         print("Arguments:")
-        # print("  > Inputfile : {}".format(self.input_file))
+        print("  > Inputfile : {}".format(self.input_word))
         print("")
 
 if __name__ == '__main__':
