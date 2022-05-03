c0d21c721dad189eb45bc9ae58a9a61491540912
Britt vink <brittvink@Britts-MacBook-Air.local>
2022-05-03 09:30:08 +0200

    adjustments in code because files are deleted

--- website/database/urls.py
+++ website/database/urls.py
@@ -7,16 +7,8 @@ from django.urls import include, re_path
 urlpatterns = [
     path('', views.home, name="home"),
     path('search_titles', views.search_titles_and_topics, name='search-titles'),
-    path('read_rss', views.read_rss, name="read-rss"),
-    path('text_field_rss/', views.text_field_rss, name="text-field-rss"),
-    path('upload/', views.upload, name='upload'),
     path('api/', views.api.as_view(), name='api'),
     path('api/<article_id>', views.api.as_view(), name="api"),
     re_path(r'^articles/$', views.show_all_articles, name='all-articles'),
     re_path(r'^article/$', views.show_article, name='article' ),
-    re_path(r'^filters/$', views.show_filters, name='all-filters'),
-    re_path(r'^articles-of-filter/$', views.show_articles_of_filter, name='show-articles-of-filter'),
-    re_path(r'^add_vocabulair/$', views.add_vocabulair, name="add-vocabulair"),
-    re_path(r'^show_vocabulair/$', views.show_vocabulair, name="show-vocabulair"),
-    path('make_filter', views.make_filter, name="make-filter")
 ]
--- website/database/views.py
+++ website/database/views.py
@@ -1,25 +1,10 @@
-from .models import Information, Search, Vocabulair
-
-from django.db import connection
+from .models import Information
 from django.db.models import Q
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.files.storage import FileSystemStorage
-from django.shortcuts import redirect, render, get_object_or_404
-
-from .read_rss_data import main
-from .put_article_in_db import main_stoppen
+from django.shortcuts import render
 from .serializers import InformationSerializers
-from .make_json import jsonmain
-from .add_word_to_vocabulaire_db import add_word_to_db
-from .webscraping import main_webscraping
-from .make_filter import main_make_filter
-
-import os
-import urllib
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+
 
 # Create your views here.
 def home(request):
@@ -63,74 +48,6 @@ def search_titles_and_topics(request):
         return render(request, 'search_for_articles.html', {})
 
 
-def read_rss(request):
-    """
-    This function redirect to either a page to upload a textfile or fill in a text field,
-    both to add data to the db
-    """
-    if request.method == "POST":
-        if request.POST.get('action') == "fill_txt_field":
-            return redirect('text-field-rss')
-        else:
-            return redirect('upload/')
-
-    return render(request, 'upload_data_to_db/rss_file.html')
-
-
-def text_field_rss(request):
-    """
-    If the text field is filled, the link will be paresed to be read and put in the database
-    """
-    context = {}
-    if request.method == "POST":
-        rss = request.POST['rss']
-
-        returnvalue = main(rss)
-
-        # If the link is valid and data is put in a text file, the file can be put in the db
-        if returnvalue == "Er is data opgehaald van de link":
-            main_stoppen()
-
-        context['return_value'] = returnvalue
-
-    return render(request, 'upload_data_to_db/text_field_rss.html', context)
-
-
-def upload(request):
-    """
-    If a file is uploaded, the links are read and data is put in the database
-    """
-    context = {}
-
-    if request.method == 'POST':
-        # Save uploaded file
-        uploaded_file = request.FILES['document']
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
-        context['url'] = fs.url(name)
-
-        cwd = os.getcwd()
-        new_added_file = os.path.join(cwd + fs.url(name))
-
-        # Read all lines in file
-        with open(new_added_file) as f:
-            lines = f.readlines()
-
-        # Add rss link by link
-        for line in lines:
-            line = line.strip()
-            # Read rss link
-            returnvalue = main(line)
-
-            # If the link is valid and data is put in a text file, the file can be put in the db
-            if returnvalue == "Er is data opgehaald van de link":
-                main_stoppen()
-
-        context['return_value'] = returnvalue
-
-    return render(request, 'upload_data_to_db/choose_upload.html', context)
-
-
 class api(APIView):
     """
     API is available, if an article id is parsed, only that information in available
@@ -172,106 +89,3 @@ def show_article(request):
 
     return render(request, 'showing_articles/show_article.html', {'article': article, 'article_text': result})
 
-
-def show_filters(request):
-    """
-    Show all filters
-    """
-    all_filters = Search.objects.all()
-    topics_list = []
-
-    for filter in all_filters:
-        if filter.search_id not in topics_list:
-            topics_list.append(filter.search_id)
-    return render(request, 'showfilter.html', {'topics_list': topics_list})
-
-
-def show_articles_of_filter(request):
-    """
-    Show all articles that are found using a filter
-    """
-    filter_ids = request.GET.getlist('item')
-    and_or = request.GET.get('type')
-
-    all_lists_with_articles = []
-
-    for filter_id in filter_ids:
-
-        # Use sql query code to find the articles that match the filter ID
-        articles = Information.objects.raw(
-                """select KCBBE2.database_Information.id 
-                from KCBBE2.database_Information 
-                inner join KCBBE2.database_Search 
-                on KCBBE2.database_Information.id=KCBBE2.database_Search.article_id 
-                where KCBBE2.database_Search.search_id = '"""
-                + filter_id + """'""")
-
-        # Articles of all filterid will be added to a list
-        all_lists_with_articles.append(articles)
-
-    # If the articles should have multiple filters, only the articles that are found for all filters should be returned
-    if and_or == 'and':
-        filtered_set = set.intersection(*[set(x) for x in all_lists_with_articles])
-        # Make list of a set
-        list_with_articles_to_return =  (list(filtered_set))
-
-    # Else return all articles, make one list of the multiple lists
-    else:
-        list_with_articles_to_return = [item for sublist in all_lists_with_articles for item in sublist]
-
-    return render(request, 'show_all_articles_of_filter.html', {'articles_list': list_with_articles_to_return})
-
-
-def add_vocabulair(request):
-    """
-    Add a word to the Vocabulaire table in the database
-    """
-
-    key = request.GET.get('key')
-    if key == None:
-        key = ""
-
-    if request.method == "POST":
-        parent = request.POST['onder']
-        child = request.POST['woord']
-
-        add_word_to_db(parent, child)
-
-        return render(request, 'add_synonym.html', {'onder': parent, 'woord': child})
-    else:
-        return render(request, 'add_synonym.html', {'key': key })
-
-
-def show_vocabulair(request):
-    all_synonymes = Vocabulair.objects.all()
-
-    list_with_all_different_synonymes = {}
-
-    for synonyme in all_synonymes:
-        if synonyme.key_id not in list_with_all_different_synonymes:
-            list_with_all_different_synonymes[synonyme.key_id] = []
-        list_with_all_different_synonymes[synonyme.key_id].append(synonyme)
-
-    return render(request, 'show_vocabulair.html', {"list_different_synonymes": list_with_all_different_synonymes})
-
-def make_text_files(request):
-    all_articles = Information.objects.all()
-    for article in all_articles:
-        print(article.id)
-
-        result = main_webscraping(article.link)
-        filename = "media/articles/" + article.id + ".txt"
-        f = open(filename, "w")
-        f.write(result)
-        f.close()
-
-def make_filter(request):
-
-    if request.method == "POST":
-        input = request.POST['filter']
-        all_articles = Information.objects.all()
-
-        main_make_filter(input, all_articles)
-        return redirect('all-filters')
-
-    return render(request, 'make_filter.html')
