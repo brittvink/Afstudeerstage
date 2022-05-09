2ba489441a77ff5ac13273c3d8e12273a9d3409f
Britt vink <brittvink@Britts-MacBook-Air.local>
2022-05-09 10:50:17 +0200

    datacleaning and making a sentiment plot

---
+++ NLP/get_cleaned_data.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import re
+import string
+
+
+def preprocess_text(text: str, remove_stopwords: bool) -> str:
+    """This utility function sanitizes a string by:
+    - removing links
+    - removing special characters
+    - removing numbers
+    - removing stopwords
+    - transforming in lowercase
+    - removing excessive whitespaces
+    Args:
+        text (str): the input text you want to clean
+        remove_stopwords (bool): whether or not to remove stopwords
+    Returns:
+        str: the cleaned text
+    """
+
+    # remove links
+    text = re.sub(r"http\S+", "", text)
+    # remove special chars and numbers
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = re.sub('[‘’“”…]', '', text)
+    text = re.sub('\n', '', text)
+    
+    # remove stopwords
+    if remove_stopwords:
+        # 1. tokenize
+        tokens = nltk.word_tokenize(text)
+        # 2. check if stopword
+        tokens = [w for w in tokens if not w.lower() in stopwords.words("english")]
+        # 3. join back together
+        text = " ".join(tokens)
+    # return text in lower case and stripped of whitespaces
+    text = text.lower().strip()
+    return text
+
+
+df = pd.read_pickle("df2.pkl")
+
+df['cleaned'] = df['text'].apply(lambda x: preprocess_text(x, remove_stopwords=True))
+
+df.to_pickle("df_cleaned.pkl")
---
+++ NLP/get_interesting_topics_in_cleaned_data.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import re
+import string
+
+df = pd.read_pickle("df_cleaned.pkl")
+print(df['rss'])
+
+topics_sentiment_food_list = [  "https://www.sciencedaily.com/rss/health_medicine/nutrition.xml",
+                                "https://www.sciencedaily.com/rss/mind_brain/consumer_behavior.xml",
+                                "https://www.sciencedaily.com/rss/plants_animals/agriculture_and_food.xml",
+                                "https://www.sciencedaily.com/rss/plants_animals/food.xml",
+                                "https://www.sciencedaily.com/rss/plants_animals/food_and_agriculture.xml",
+                              ]
+
+df_sentiment_food = df.loc[df['rss'].isin(topics_sentiment_food_list)]
+print(df_sentiment_food)
+df_sentiment_food.to_pickle("df_sentiment_food.pkl")
+
+topics_sentiment_energy_list = ["https://www.sciencedaily.com/rss/matter_energy/energy_and_resources.xml",
+                                "https://www.sciencedaily.com/rss/matter_energy/energy_policy.xml",
+                                "https://www.sciencedaily.com/rss/matter_energy/nuclear_energy.xml",
+                                "https://www.sciencedaily.com/rss/matter_energy/alternative_fuels.xml",
+                                "https://www.sciencedaily.com/rss/earth_climate/renewable_energy.xml",
+                                "https://www.sciencedaily.com/rss/science_society/energy_issues.xml",
+                                "https://www.sciencedaily.com/rss/matter_energy/fossil_fuels.xml",
+                                "https://www.sciencedaily.com/rss/strange_offbeat/fossils_ruins.xml"]
+
+df_sentiment_energy = df.loc[df['rss'].isin(topics_sentiment_energy_list)]
+print(df_sentiment_energy)
+df_sentiment_energy.to_pickle("df_sentiment_energy.pkl")
+
+
+topics_kcbbe_list = ["https://www.sciencedaily.com/rss/space_time/solar_system.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/energy_and_resources.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/biochemistry.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/inorganic_chemistry.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/organic_chemistry.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/chemistry.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/electricity.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/energy_policy.xml"
+                "https://www.sciencedaily.com/rss/matter_energy/wind_energy.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/batteries.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/nuclear_energy.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/fuel_cells.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/energy_technology.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/alternative_fuels.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/fossil_fuels.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/solar_energy.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/civil_engineering.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/forensics.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/engineering.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/materials_science.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/quantum_computing.xml",
+                "https://www.sciencedaily.com/rss/computers_math/computational_biology.xml",
+                "https://www.sciencedaily.com/rss/computers_math/computer_modeling.xml",
+                "https://www.sciencedaily.com/rss/computers_math/computer_science.xml",
+                "https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml",
+                "https://www.sciencedaily.com/rss/computers_math/computer_programming.xml",
+                "https://www.sciencedaily.com/rss/computers_math/information_technology.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/genetically_modified.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/pests_and_parasites.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/cloning.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/drought.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/soil_types.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/bird_flu.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/agriculture_and_food.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/food.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/organic.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/biotechnology_and_bioengineering.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/food_and_agriculture.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/life_sciences.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/biochemistry.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/biotechnology.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/cell_biology.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/microbes_and_more.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/microbiology.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/viruses.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/bacteria.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/fungus.xml",
+                "https://www.sciencedaily.com/rss/plants_animals/prions.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/renewable_energy.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/geoengineering.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/recycling_and_waste.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/mining.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/climate.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/earth_science.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/chemistry.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/environmental_awareness.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/drought.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/air_pollution.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/environmental_policy.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/oil_spills.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/acid_rain.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/global_warming.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/ozone_holes.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/pollution.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/hazardous_waste.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/air_quality.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/environmental_issues.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/sustainability.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/energy.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/ecology.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/water.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/coral_reefs.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/rainforests.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/wildfires.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/ecosystems.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/environmental_science.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/biodiversity.xml",
+                "https://www.sciencedaily.com/rss/earth_climate/near-earth_object_impacts.xml",
+                "https://www.sciencedaily.com/rss/science_society/energy_issues.xml",
+                "https://www.sciencedaily.com/rss/science_society/ocean_policy.xml",
+                "https://www.sciencedaily.com/rss/science_society/resource_shortage.xml",
+                "https://www.sciencedaily.com/rss/science_society/bioethics.xml",
+                "https://www.sciencedaily.com/rss/science_society/disaster_plan.xml",
+                "https://www.sciencedaily.com/rss/science_society/political_science.xml",
+                "https://www.sciencedaily.com/rss/matter_energy/energy_and_resources.xml",
+                "https://www.sciencedaily.com/rss/strange_offbeat/matter_energy.xml",
+                "https://www.sciencedaily.com/rss/strange_offbeat/earth_climate.xml",
+                "https://www.sciencedaily.com/rss/strange_offbeat/fossils_ruins.xml"]
+
+df_topics_kcbbe = df.loc[df['rss'].isin(topics_kcbbe_list)]
+print(df_topics_kcbbe)
+df_topics_kcbbe.to_pickle("df_topics_kcbbe.pkl")
+
