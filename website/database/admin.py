c7d19f899ebade0c40811e4623dffba33d5c6740
Britt vink <brittvink@Britts-MacBook-Air.local>
2022-06-10 11:25:28 +0200

    merge errors

--- website/database/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-
-from .models import Information, Search
-
-admin.site.register(Information)
-admin.site.register(Search)
--- website/database/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Information
-
-class InformationSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Information
-        fields = '__all__'
